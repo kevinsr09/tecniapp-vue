f54862c39ebfc1f9edcebbdecec2d1a757374244
kevinsr09 <kevinstivenrugeles@gmail.com>
2023-07-05 10:50:19 -0500

    categorias con open ai

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -3,27 +3,80 @@ export default{
   name:"MainHeader",
   data(){
     return {
-      search:"",
-    }
+      userProblem:"",
+      searchCategories:""
+    }      
+    
   },
-  methods:{
-    alerta(){
-      alert(this.search)
 
+  computed:{
+    
+    optionsUser(){
+
+      return{
+        "model": "gpt-3.5-turbo",
+      "messages": [
+        {
+          "role": "system",
+          "content": `clasifica el problema del usuario en las siguientes categorias: reparacion de televisores, reparacion de neveras, reparacion de estufas, reparacion de lavadoras, reparacion de computadores, reparacion de telefonos, accesorios para telefonos. solo respondes con las categorias en un array`
+        },
+        {
+          "role": "user",
+          "content": `${this.userProblem}`
+        }
+      ]
+      }
+      
+    },
+
+    optionsSend(){
+
+      return{
+        method:'POST',
+        body: JSON.stringify({
+          "model": "gpt-3.5-turbo",
+          "messages": [
+            {
+              "role": "system",
+              "content": `clasifica el problema del usuario en las siguientes categorias: reparacion de televisores, reparacion de neveras, reparacion de estufas, reparacion de lavadoras, reparacion de computadores, reparacion de telefonos, accesorios para telefonos. solo respondes con las categorias en un array`
+            },
+            {
+              "role": "user",
+              "content": `${this.userProblem}`
+            }
+          ]
+        }),
+
+        headers: {
+        
+          "Content-Type": "application/json",
+          "Authorization": "Bearer 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"
+        }
+      }
+      
     }
-  }
 
+    
+  },
+
+  methods:{
+    userCategories(){
+      console.log(this.optionsSend)
+      fetch('https://api.openai.com/v1/chat/completions', this.optionsSend)
+        .then(res => res.json())
+        .then(data =>  this.searchCategories = data.choices[0].message.content)
+      
+        setTimeout(()=>{console.log(this.searchCategories)},2000)
+    },
+  }
 }
 
 
-/*
- background-color: #444343;
-  border-radius: 0.5rem;
-  transition: background-color 0.1s ease-in-out;
-  font-size: 1.5rem;
-  align-items: center;
-*/
 </script>
+
+
+
+
 
 <template>
   <header>
@@ -32,12 +85,12 @@ export default{
 
     <div class="header-div search">
     
-      <form action="" @submit.prevent="alerta" class="form-search">
+      <form action="" @submit.prevent="userCategories" class="form-search">
         
-        <input type="text" v-model="search" placeholder="Cuentanos que te sucedio" class="form-search-text">
+        <input type="text" v-model="userProblem" placeholder="Cuentanos que te sucedio" class="form-search-text">
         
         <button type="submit" class="form-search-submit">
-          <img src="svg/search.svg" alt="svg">
+          <img src="/svg/search.svg" alt="svg">
         </button>
       
       </form> 
